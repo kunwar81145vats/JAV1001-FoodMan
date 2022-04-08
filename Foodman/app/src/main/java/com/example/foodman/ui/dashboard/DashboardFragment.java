c1785e1fe436dcbef8583c1c5742aab264cce4e0
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-04-08 17:43:35 +0530

    Commit details: 1. Favourites page implemented.

--- Foodman/app/src/main/java/com/example/foodman/ui/dashboard/DashboardFragment.java
+++ Foodman/app/src/main/java/com/example/foodman/ui/dashboard/DashboardFragment.java
@@ -11,30 +11,32 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodman.HomeAdapter;
 import com.example.foodman.R;
 import com.example.foodman.databinding.FragmentDashboardBinding;
+import com.example.foodman.ui.home.CommonSingleton;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
+    private RecyclerView itemsRecyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        itemsRecyclerView = binding.FavItemsRecyclerView;
+
+        FavouritesAdapter homeAdapter = new FavouritesAdapter(getActivity().getApplicationContext(), CommonSingleton.favItems);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        itemsRecyclerView.setLayoutManager(linearLayoutManager);
+        itemsRecyclerView.setAdapter(homeAdapter);
+
         return root;
     }
 
